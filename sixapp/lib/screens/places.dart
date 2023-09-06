0b3bb786fba4def4e6fe3fc142514ad9530e8eab
victorrsouzas <victorrsouzas@gmail.com>
2023-09-06 15:58:55 -0300

    Finalization project FavoritePlacesApp

---
+++ sixapp/lib/screens/places.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sixapp/providers/user_places.dart';
+import 'package:sixapp/screens/add_place.dart';
+import 'package:sixapp/widgets/places_list.dart';
+
+class PlacesScreen extends ConsumerStatefulWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesSreenState();
+  }
+}
+
+class _PlacesSreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPLaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlacesList(places: userPlaces),
+        ),
+      ),
+    );
+  }
+}
